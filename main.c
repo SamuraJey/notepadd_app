a88e000d574fddd28bbc6ed45dc0673f1b8bad07
alexg0219 <dexpsnapm245@gmail.com>
2024-10-24 17:51:55 +0500

    CODE-13: window size, easy way

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 struct termios orig_termios;
 
 struct editorConfig {
     struct termios orig_termios;
+    int screenrows;
+    int screencols;
 };
 struct editorConfig E;
 
@@ -32,8 +35,31 @@ void enableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
