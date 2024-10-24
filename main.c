1c77d66dc3f24246b3d3c978810f5fe1c9311b61
alexg0219 <dexpsnapm245@gmail.com>
2024-10-24 18:49:07 +0500

    CODE-17: Hide cursor when repainting CODE-18: Clear lines one at time CODE-19: Welcom message CODE-20: Move cursor CODE-21: Arrow keys

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
+#define NOTEPADD_APP_VERSION "0.0.1"
+
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
+
 
 struct termios orig_termios;
 
 struct editorConfig {
     struct termios orig_termios;
-  
+    int cx, cy;
     int screenrows;
     int screencols;
 
 };
 struct editorConfig E;
+
+struct abuf {
+    char *b;
+    int len;
+};
 
 void die(const char *s) {
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -49,18 +67,135 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 void editorDrawRows() {
     int y;
-    
+
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
         
-        if (y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+        if (y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+            "Notepadd app -- version %s", NOTEPADD_APP_VERSION);
+
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+
+        int padding = (E.screencols - welcomelen) / 2;
+
+        if (padding) {
+            abAppend(ab, "~", 1);
+            padding--;
         }
+
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+
+        } else {
+        abAppend(ab, "~", 1);
+        }
+
+        abAppend(ab, "\x1b[K", 3);
+        if (y < E.screenrows - 1) {
+        abAppend(ab, "\r\n", 2);
+        }
+    }
+
+}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+void editorRefreshScreen() {
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[?25l", 6);
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+void editorMoveCursor(char key) {
+    switch (key) {
+        case ARROW_LEFT:
+            E.cx--;
+            break;
+        case ARROW_RIGHT:
+            E.cx++;
+            break;
+        case ARROW_UP:
+            E.cy--;
+            break;
+        case ARROW_DOWN:
+            E.cy++;
+        break;
+    }
+}
+
+void editorProcessKeypress() {
+    int c = editorReadKey();
+    
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
+  }
+}
+
+int editorReadKey() {
+    int nread;
+    char c;
+    
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    
+    if (c == '\x1b') {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A': return 'w';
+                case 'B': return 's';
+                case 'C': return 'd';
+                case 'D': return 'a';
+            }
+        }
+        return '\x1b';
+    } else {
+        return c;
     }
 }
 
